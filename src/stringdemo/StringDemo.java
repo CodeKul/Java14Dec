1126981ed1fc6f847c61ad59429a1f3301591cff
Vaibhav N <vn202207@nexttechvision.com>
2023-02-21 18:29:56 +0530

    String

---
+++ src/stringdemo/StringDemo.java
@@ -0,0 +1,42 @@
+package stringdemo;
+
+public class StringDemo {
+    public static void main(String[] args) {
+
+        String str = "I am Indian" +
+                      "fe;wnr am lrewn" +
+                      "rerewrrwerer" +
+                      "werwrewrewre";
+
+
+        String str1 = "pune";
+
+        String str3 = "we"+9+"ll"+8+9+"c"+8+"ome"+10;
+
+        System.out.println(str3);
+
+        System.out.println(str==str1);
+        System.out.println(str.equalsIgnoreCase(str1));
+
+//        String str1= new String("Pune");
+
+//        str =str.concat("Kolhapur");
+//
+//        System.out.println(str);
+//        System.out.println(str.charAt(2));
+//        System.out.println(str.indexOf('n'));
+//        System.out.println(str.isBlank());
+//        System.out.println(str.isEmpty());
+//        System.out.println(str.chars().count());
+
+
+//        String obj1 = "Obj1";
+//        String obj2 = "Obj1";
+//        String obj3 = "Obj1";
+//        String obj4 = "Obj1";
+//
+//        char[] arr = {'w','e','l','l'};
+//        String str1 = new String(arr);
+//        System.out.println(str1);
+    }
+}
